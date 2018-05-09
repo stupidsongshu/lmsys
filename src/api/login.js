5b216924dc9fb43880c2d78b7f678782ed58b9ec
yusong <330730263@qq.com>
2018-05-09 23:57:22 +0800

    login interface modify

--- src/api/login.js
+++ src/api/login.js
@@ -1,7 +1,7 @@
 import httpSign from '@/utils/http'
 
-export function login(userInfo) {
+export function login(data) {
   let url = '/lm/c/i'
   let call = 'Account.login'
-  return httpSign(url, call, userInfo)
+  return httpSign({url, call, data})
 }
