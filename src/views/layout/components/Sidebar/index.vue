0510f3465d08d74c9cc62d3b522bddac25fb4fa8
stupidSquirrel <zys19920204@gmail.com>
2018-07-23 00:34:58 +0800

    修复多层嵌套路由sidebar展示bug

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -1,25 +1,33 @@
 <template>
-  <scroll-bar>
-    <el-menu mode="vertical" unique-opened :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#fff" active-text-color="#409EFF">
-      <sidebar-item :routes="routes"></sidebar-item>
+  <el-scrollbar wrapClass="scrollbar-wrapper">
+    <el-menu
+      mode="vertical"
+      :show-timeout="200"
+      :default-active="$route.path"
+      :collapse="isCollapse"
+      background-color="#304156"
+      text-color="#bfcbd9"
+      active-text-color="#409EFF"
+    >
+      <sidebar-item v-for="route in routes" :key="route.name" :item="route" :base-path="route.path"></sidebar-item>
     </el-menu>
-  </scroll-bar>
+  </el-scrollbar>
 </template>
 
 <script>
+import { routes } from '@/router'
 import { mapGetters } from 'vuex'
-import ScrollBar from '@/components/ScrollBar'
 import SidebarItem from './SidebarItem'
 
 export default {
-  components: { ScrollBar, SidebarItem },
+  components: { SidebarItem },
   computed: {
-    ...mapGetters ([
+    routes() {
+      return routes
+    },
+    ...mapGetters([
       'sidebar'
     ]),
-    routes() {
-      return this.$router.options.routes
-    },
     isCollapse() {
       return !this.sidebar.opened
     }
